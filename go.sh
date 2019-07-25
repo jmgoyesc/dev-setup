4ff10a97183db96a7a765ebced58b53ab8adc76d
jmgoyesc <jmgoyesc@users.noreply.github.com>
2019-07-25 15:42:12 +0200

    Go tools

---
+++ go.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Install Go
+mkdir -p ~/dev/go
+export GOPATH=~/dev/go
+export PATH=$PATH:$GOPATH/bin
+brew install go
+
+# GO tools and dep
+brew install dep
+go get golang.org/x/tools/cmd/godoc
+go get github.com/golang/lint/golint
+
+# Remove outdated versions from the cellar.
+brew cleanup
