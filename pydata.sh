3b28f99954a92591454d6b67179af46cce96aac8
jmgoyesc <jmgoyesc@users.noreply.github.com>
2019-07-25 20:51:26 +0200

    Ignore python data tools

--- pydata.sh
+++ pydata.sh
@@ -50,20 +50,20 @@ mkvirtualenv py2-data
 workon py2-data
 
 # Install Python data modules
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install sympy
-pip install nose
-pip install unittest2
-pip install seaborn
-pip install scikit-learn
-pip install "ipython[all]"
-pip install bokeh
-pip install Flask
-pip install sqlalchemy
-pip install mysql-python
+#pip install numpy
+#pip install scipy
+#pip install matplotlib
+#pip install pandas
+#pip install sympy
+#pip install nose
+#pip install unittest2
+#pip install seaborn
+#pip install scikit-learn
+#pip install "ipython[all]"
+#pip install bokeh
+#pip install Flask
+#pip install sqlalchemy
+#pip install mysql-python
 
 ###############################################################################
 # Python 3 Virtual Enviroment                                                 #
@@ -77,21 +77,21 @@ mkvirtualenv --python=/usr/local/bin/python3 py3-data
 workon py3-data
 
 # Install Python data modules
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install sympy
-pip install nose
-pip install unittest2
-pip install seaborn
-pip install scikit-learn
-pip install "ipython[all]"
-pip install bokeh
-pip install Flask
-pip install sqlalchemy
+#pip install numpy
+#pip install scipy
+#pip install matplotlib
+#pip install pandas
+#pip install sympy
+#pip install nose
+#pip install unittest2
+#pip install seaborn
+#pip install scikit-learn
+#pip install "ipython[all]"
+#pip install bokeh
+#pip install Flask
+#pip install sqlalchemy
 #pip install mysql-python  # Python 2 only, use mysqlclient instead
-pip install mysqlclient
+#pip install mysqlclient
 
 ###############################################################################
 # Install IPython Profile
@@ -101,8 +101,8 @@ echo "------------------------------"
 echo "Installing IPython Notebook Default Profile"
 
 # Add the IPython profile
-mkdir -p ~/.ipython
-cp -r init/profile_default/ ~/.ipython/profile_default
+#mkdir -p ~/.ipython
+#cp -r init/profile_default/ ~/.ipython/profile_default
 
 # Install jq
 brew install jq
